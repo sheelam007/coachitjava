3fd038d2e807b24501cd04120cfc0521cae4c5b7
sheel <sheel@LAPTOP-JSEHPTDG>
2022-07-02 23:24:16 +0530

    commit

--- coachitjava/src/Stringstask/Contains.java
+++
@@ -1,9 +0,0 @@
-package Stringstask;
-
-public class Contains {
-
-	public static void main(String[] args) {
-		String name="my name is sandhya";
-		System.out.println(name.contains("name is"));
-	}
-}
